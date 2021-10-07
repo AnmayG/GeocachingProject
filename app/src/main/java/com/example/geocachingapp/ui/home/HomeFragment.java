a3188ceb16a73f3bf4a8691c5b7a787027c2067f
Anmay Gupta <anmaygupta1@gmail.com>
2021-10-07 06:54:02 -0500

    add recyclerview

--- app/src/main/java/com/example/geocachingapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/geocachingapp/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.geocachingapp.ui.home;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,14 +12,22 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.geocachingapp.R;
 import com.example.geocachingapp.databinding.FragmentHomeBinding;
+import com.example.geocachingapp.ui.home.placeholder.PlaceholderContent;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+
+    public HomeFragment() {
+        // Empty fragment for free instantiation
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -35,7 +44,42 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        RecyclerView view = binding.list;
+
+        // Set the adapter
+        Context context = view.getContext();
+        if (mColumnCount <= 1) {
+            view.setLayoutManager(new LinearLayoutManager(context));
+        } else {
+            view.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+        }
+        view.setAdapter(new LocationInfoRecyclerViewAdapter(PlaceholderContent.ITEMS));
         return root;
+    }
+
+    // TODO: Customize parameter argument names
+    private static final String ARG_COLUMN_COUNT = "column-count";
+    // TODO: Customize parameters
+    private int mColumnCount = 1;
+
+    // TODO: Customize parameter initialization
+    @SuppressWarnings("unused")
+    public static LocationInfoFragment newInstance(int columnCount) {
+        LocationInfoFragment fragment = new LocationInfoFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_COLUMN_COUNT, columnCount);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
+        }
     }
 
     @Override
